2c49efaff88a78082cd879367446d70c5c9c7ed7
Alex Matskevich <41exmackevich@gmail.com>
2013-09-20 17:01:55 +0000

    Edited kaminari_config.rb

--- config/initializers/kaminari_config.rb
+++ config/initializers/kaminari_config.rb
@@ -3,7 +3,7 @@ Kaminari.configure do |config|
   # config.max_per_page = nil
   # config.window = 4
   # config.outer_window = 0
-   config.left = 1
+  # config.left = 1
   # config.right = 0
   # config.page_method_name = :page
   # config.param_name = :page
