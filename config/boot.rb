0af6d71329cdac9ed4ab26dacd596f9ed2ddc747
Alex <41exmackevich@gmail.com>
2013-09-23 20:38:26 +0300

    first commit

--- config/boot.rb
+++ config/boot.rb
@@ -2,3 +2,4 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
+ENV['NLS_LANG'] = 'AMERICAN_AMERICA.WE8ISO8859P15'
