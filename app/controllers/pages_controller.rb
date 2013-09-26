63c76d34e4d5e935ecbe1063f54c8cb7519c2cd3
Alex <41exmackevich@gmail.com>
2013-09-26 16:29:23 +0300

    Added options. Updated new action.Created show action

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,10 +10,11 @@ end
 def create
 	@page = Page.new(params[:page])
 	  if @page.save
-			redirect_to page_path(@page)
-		else
-			render 'new'
-		end
+	  	flash[:success] = "Page:#{@page.page_title} was created!" 
+	  	redirect_to pages_path
+	  else
+	  	render 'new'
+	end
 end
 
 def new
