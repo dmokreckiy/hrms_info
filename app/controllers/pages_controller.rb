87921e692228d4b663f08b1c3bbf39d9adcc1df3
Alex <41exmackevich@gmail.com>
2013-10-17 16:31:22 +0300

    Added create action for page. Updated view for new page, custom.css.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,33 +1,33 @@
 class PagesController < ApplicationController
 
 
-def index
-	@pages = Page.page params[:page]
-end
-
-def create
-	@page = Page.new(params[:page])
-	  if @page.save
-	  	flash[:success] = "The #{@page.page_title} has been saved" 
-	  	redirect_to pages_path
-	  else
-	  	render 'new'
+	def index
+		@pages = Page.page params[:page]
 	end
-end
 
-def new
-	@page = Page.new
-end
+	def create
+		@page = Page.create(params[:page])
+			if @page.errors.empty?
+				flash[:success] = "The #{@page.page_title} has been saved" 
+				redirect_to pages_path
+			else
+				redirect_to new_page_path(@page)
+			end
+	end
 
-def show
-	@page = Page.find(params[:id])
-end
+	def new
+		@page = Page.new
+	end
 
-def destroy 
-	@page = Page.find(params[:id])
-	@page.destroy
-	redirect_to action: "index"
-end
+	def show
+		@page = Page.find(params[:id])
+	end
+
+	def destroy 
+		@page = Page.find(params[:id])
+		@page.destroy
+		redirect_to action: "index"
+	end
 
 end
 
--- app/models/page.rb
+++ app/models/page.rb
@@ -1,7 +1,7 @@
 class Page < ActiveRecord::Base
 
   VALID_NAME_REGEX = /[a-zA-Z0-9+\'\"\.\,\:\;\-]/
-  # attr_accessible :page_title, :page_url, :keywords, :description, :content, :parent_page_id, :page_type, :display_top_menu, :display_bottom_menu, :published
+  attr_accessible :page_title, :page_url, :keywords, :description, :content, :parent_page_id, :page_type, :display_top_menu, :display_bottom_menu, :published
   validates :page_title, presence: true, format: { with: VALID_NAME_REGEX }, length: { minimum: 3, maximum: 50 }
   validates :page_url, format: { with: VALID_NAME_REGEX }, presence: true
   validates :description, length: { maximum: 200 }, format: { with: VALID_NAME_REGEX }
