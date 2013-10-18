49dae264718556d9621c0862d7fadece3455e98b
Alex Matskevich <41exmackevich@gmail.com>
2013-10-18 12:22:37 +0000

    Pages_controller.rb edited.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,33 +1,32 @@
 class PagesController < ApplicationController
 
+  def index
+    @pages = Page.page params[:page]
+  end
 
-	def index
-		@pages = Page.page params[:page]
-	end
+  def create
+    @page = Page.create(params[:page])
+    if @page.errors.empty?
+      flash[:success] = "The #{@page.page_title} has been saved" 
+      redirect_to pages_path
+    else
+      redirect_to new_page_path(@page)
+    end
+  end
 
-	def create
-		@page = Page.create(params[:page])
-			if @page.errors.empty?
-				flash[:success] = "The #{@page.page_title} has been saved" 
-				redirect_to pages_path
-			else
-				redirect_to new_page_path(@page)
-			end
-	end
+  def new
+    @page = Page.new
+  end
 
-	def new
-		@page = Page.new
-	end
+  def show
+    @page = Page.find(params[:id])
+  end
 
-	def show
-		@page = Page.find(params[:id])
-	end
-
-	def destroy 
-		@page = Page.find(params[:id])
-		@page.destroy
-		redirect_to action: "index"
-	end
+  def destroy 
+    @page = Page.find(params[:id])
+    @page.destroy
+    redirect_to action: "index"
+  end
 
 end
 
