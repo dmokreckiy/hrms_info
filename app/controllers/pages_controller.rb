b6ef576073137e8acb22758db263ece7ed443396
Denis Mokreckiy <d.mokreckiy@gmail.com>
2013-10-19 13:57:15 +0300

    Add filter and sorting prototype

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,7 @@ class PagesController < ApplicationController
 
   def index
     @pages = Page.paginate(page: params[:page],:per_page => 20)
-    sorting(params[:sort], params[:order])
+    sort_and_filter(params[:sort], params[:order], params[:filter])
   end
 
   def create
@@ -34,19 +34,25 @@ class PagesController < ApplicationController
   private
 
   #сортировка по умолчания - created desc - от нового к старым значениям
-  def sorting (column_name = 'created', order_type = 'desc')
+  def sorting (column_name = 'created', order_type = 'desc', filter = nil)
+    unless filter.nil?
+        @filtered = @pages.where("page_title LIKE INITCAP('%#{filter}%')")
+      else
+        @filtered = @pages
+    end
+    
     case column_name
     when 'title'
       if order_type == 'asc'
-        @pages.sort_by!{ |p| p.page_title }
+        @filtered.sort_by!{ |p| p.page_title }
       elsif order_type == 'desc'
-        (@pages.sort_by!{ |p| p.page_title }).reverse
+        (@filtered.sort_by!{ |p| p.page_title }).reverse
       end
     when 'created'
       if order_type == 'asc'
-        @pages.sort_by!{ |p| p.created_at }
+        @filtered.sort_by!{ |p| p.created_at }
       elsif order_type == 'desc'
-        (@pages.sort_by!{ |p| p.created_at }).reverse
+        (@filtered.sort_by!{ |p| p.created_at }).reverse
       end
     end
   end
