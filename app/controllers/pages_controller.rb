bc23e6a08b6815ead32254998214bc37e8cec3ca
Alex <41exmackevich@gmail.com>
2013-11-06 01:27:28 +0300

    Added view edit. Added action edit and update

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,33 +1,48 @@
 class PagesController < ApplicationController
 
 
-	def index
-		@pages = Page.page params[:page]
-	end
+  def index
+    @pages = Page.page params[:page]
+  end
 
-	def create
-		@page = Page.create(params[:page])
-		if @page.errors.empty?
-			flash[:success] = "The #{@page.page_title} has been saved" 
-			redirect_to pages_path
-		else
-			redirect_to new_page_path(@page)
-	  	flash[:failure] = "Page save failed"
-		end
-	end
+  def create
+    @page = Page.create(params[:page])
+    if @page.errors.empty?
+      flash[:success] = "The #{@page.page_title} has been saved" 
+      redirect_to pages_path
+    else
+      redirect_to new_page_path(@page)
+      flash[:failure] = "Page save failed"
+    end
+  end
 
-	def new
-		@page = Page.new
-	end
+  def edit 
+    @page = Page.find(params[:id])
+  end
 
-	def show
-		@page = Page.find(params[:id])
-	end
+  def update
+    @page = Page.find(params[:id])
+    @page.update_attributes(params[:page])
+      if @page.errors.empty?
+        flash[:success] = "The #{@page.page_title} has been updated"
+        redirect_to pages_path
+      else
+        render 'edit'
+      end
+  end
+ 
+  def new
+    @page = Page.new
+  end
 
-	def destroy 
-		@page = Page.find(params[:id])
-		@page.destroy
-		redirect_to action: "index"
-	end
+  def show
+    @page = Page.find(params[:id])
+  end
+
+  def destroy 
+    @page = Page.find(params[:id])
+    @page.destroy
+    redirect_to action: "index"
+  end
 
 end
