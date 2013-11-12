c70dc349f444f837d439f71ce1e1f16d97a66e5b
Alex <41exmackevich@gmail.com>
2013-11-12 20:54:53 +0300

    Converted new.html in haml

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,31 +5,31 @@ class PagesController < ApplicationController
     @assets = @page_grid.assets.paginate(:page => params[:page], :per_page => 20)
   end
 
-	
+  
 
-	def create
-		@page = Page.create(params[:page])
-		if @page.errors.empty?
-			flash[:success] = "The #{@page.page_title} has been saved" 
-			redirect_to pages_path
-		else
-			redirect_to new_page_path(@page)
-	  	flash[:failure] = "Page save failed"
-		end
-	end
+  def create
+    @page = Page.create(params[:page])
+    if @page.errors.empty?
+      flash[:success] = "The #{@page.page_title} has been saved" 
+      redirect_to pages_path
+    else
+      redirect_to new_page_path(@page)
+      flash[:failure] = "Page save failed"
+    end
+  end
 
-	def new
-		@page = Page.new
-	end
+  def new
+    @page = Page.new
+  end
 
-	def show
-		@page = Page.find(params[:id])
-	end
+  def show
+    @page = Page.find(params[:id])
+  end
 
-	def destroy 
-		@page = Page.find(params[:id])
-		@page.destroy
-		redirect_to action: "index"
-	end
+  def destroy 
+    @page = Page.find(params[:id])
+    @page.destroy
+    redirect_to action: "index"
+  end
 
 end
