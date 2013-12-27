8125df729bcc19ff748f593266fa02e9a1f6835d
Denis Mokreckiy <d.mokreckiy@gmail.com>
2013-12-27 16:22:22 +0300

    Removed competences API traces

--- app/controllers/api_controller.rb
+++
@@ -1,8 +0,0 @@
-class ApiController < ApplicationController
-  
-  @@competences_json = File.read(Rails.root.join('app/assets/competences.json'))
-
-  def competences
-    render :json => @@competences_json
-  end
-end
