8c13c71f37b3f7eefecc93537398e39b3d940006
Denis Mokreckiy <d.mokreckiy@gmail.com>
2014-01-15 20:53:49 +0300

    Added separate RegEx for url, changed default for page_url

--- app/models/page.rb
+++ app/models/page.rb
@@ -3,22 +3,24 @@
 # Table name: pages
 #
 #  id                  :integer          not null, primary key
-#  page_title          :string(255)
-#  page_url            :string(255)
-#  keywords            :text
-#  description         :string(255)
-#  content             :text
-#  parent_page_id      :integer
-#  page_type           :integer
-#  display_top_menu    :boolean
-#  display_bottom_menu :boolean
-#  published           :boolean
-#  created_at          :datetime
-#  updated_at          :datetime
+#  page_title          :string(255)      название страницы
+#  page_url            :string(255)      url страницы
+#  keywords            :text             ключевые слова HTML метаданных
+#  description         :string(255)      описание страницы HTML метаданных
+#  content             :text             содержание
+#  parent_page_id      :integer          id родительской страницы
+#  page_type           :integer          тип страницы
+#  display_top_menu    :boolean          признак отображения в верхнем меню
+#  display_bottom_menu :boolean          признак отображения в нижнем меню
+#  published           :boolean          признак публикации страницы
+#  created_at          :datetime         дата создания
+#  updated_at          :datetime         дата обновления
+#  ancestry            :string(255)      родительская цепочка отношений страниц
 #
 
 class Page < ActiveRecord::Base
-  VALID_NAME_REGEX = /\A[a-zA-Z0-9+\'\"\.\,\:\;\-\/]*[a-zA-Z][a-zA-Z0-9+\'\"\.\,\:\;\-\/]*\z/
+  VALID_NAME_REGEX = /\A[a-zA-Z0-9+\'\"\.\,\:\;\-\s]*[a-zA-Z\s][a-zA-Z0-9+\'\"\.\,\:\;\-\s]*\z/
+  VALID_URL_REGEX = /\A[a-zA-Z0-9+\'\"\.\,\:\;\-\/\s]*[a-zA-Z\s][a-zA-Z0-9+\'\"\.\,\:\;\-\/\s]*\z/
   attr_accessible :page_title, :page_url, :keywords, :description, :content, :parent_page_id, :page_type, :display_top_menu, :display_bottom_menu, :published
 # чистка пробелов(в начале, в конце, несколько пробелов подряд в середине), переводов строки и табуляции
   before_save do
@@ -34,13 +36,13 @@ class Page < ActiveRecord::Base
       @page_def_num = Page.last.id
     end
     self.page_title = "Page#{@page_def_num+1}" if self.page_title.blank?
-    self.page_url = "page-name-#{@page_def_num+1}" if self.page_url.blank?
+    self.page_url = "/page#{@page_def_num+1}" if self.page_url.blank?
   end
   
 # валидации на наличие заголовка страницы, соответствии формату и требованиям по размеру (мин 3, макс 50)
-  validates :page_title, presence: true, format: { with: VALID_NAME_REGEX }, length: { minimum: 3, maximum: 50 }
+  validates :page_title, presence: true, format: { with: VALID_NAME_REGEX }, length: { minimum: 3, maximum: 50 }, uniqueness: true
 # валидации на наличие ссылки страницы, соответствии формату 
-  validates :page_url, presence: true, format: { with: VALID_NAME_REGEX }
+  validates :page_url, presence: true, format: { with: VALID_URL_REGEX }, uniqueness: true
 # валидации на длину описания 
   validates :description, length: { maximum: 200 }
 end
