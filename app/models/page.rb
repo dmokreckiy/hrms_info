025f71f6e71ace880c6a292b8e3eb058a3f2c96f
Denis Mokreckiy <d.mokreckiy@gmail.com>
2014-01-27 15:54:47 +0300

    Added restriction for page deletion with child pages. Added sorting for ancestry select box

--- app/models/page.rb
+++ app/models/page.rb
@@ -3,27 +3,33 @@
 # Table name: pages
 #
 #  id                  :integer          not null, primary key
-#  page_title          :string(255)
-#  page_url            :string(255)
-#  keywords            :text
-#  description         :string(255)
-#  content             :text
-#  parent_page_id      :integer
-#  page_type           :integer
-#  display_top_menu    :boolean
-#  display_bottom_menu :boolean
-#  published           :boolean
-#  created_at          :datetime
-#  updated_at          :datetime
+#  page_title          :string(255)      page title
+#  page_url            :string(255)      page url
+#  keywords            :text             keywords for HTML metadata
+#  description         :string(255)      description for HTML metadata
+#  content             :text             page content
+#  parent_page_id      :integer          parent page id (for view and reference)
+#  page_type           :integer          page type id (view types)
+#  display_top_menu    :boolean          display at top propetry
+#  display_bottom_menu :boolean          display at bottom property
+#  published           :boolean          publish property
+#  created_at          :datetime         self explanatory
+#  updated_at          :datetime         self explanatory
+#  ancestry            :string(255)      Ancestry string for tree structure (Parent Page function)
 #
 
 class Page < ActiveRecord::Base
   has_ancestry
   
+  def apply_orphan_strategy
+    # Destruction will be handled separately
+  end
+
+#  RegEx for page title and page url validation  
   VALID_NAME_REGEX = /\A[a-zA-Z0-9+\'\"\.\,\:\;\-\s]*[a-zA-Z\s][a-zA-Z0-9+\'\"\.\,\:\;\-\s]*\z/
   VALID_URL_REGEX = /\A[a-zA-Z0-9+\'\"\.\,\:\;\-\/\s]*[a-zA-Z\s][a-zA-Z0-9+\'\"\.\,\:\;\-\/\s]*\z/
   attr_accessible :page_title, :page_url, :keywords, :description, :content, :parent_page_id, :page_type, :display_top_menu, :display_bottom_menu, :published
-# чистка пробелов(в начале, в конце, несколько пробелов подряд в середине), переводов строки и табуляции
+# whitespace cleaning
   before_save do
     self.page_title.squish!
     self.description.squish! unless self.description.blank?
@@ -32,32 +38,35 @@ class Page < ActiveRecord::Base
     self.published = false if self.published.nil?
     unless self.parent_page_id.blank?
       if self.id.nil?
-          self.parent_id = Page.find(self.parent_page_id) 
+        self.parent_id = Page.find(self.parent_page_id) 
       else
         self.parent_id = Page.find(self.parent_page_id) unless self.parent_page_id == Page.find(self.id).parent_page_id            
       end
     end
   end
 
-# вставка значений по умолчанию в поля page_title и page_url пустые в форме
+# default value insertion for page title and page url (mandatory fields)
   before_validation do
     if Page.last.nil?
-      @page_def_num = 1
+      @page_def_num = 0
     else
       @page_def_num = Page.last.id
     end
     self.page_title = "Page#{@page_def_num+1}" if self.page_title.blank?
-    self.page_url = "/page-#{@page_def_num+1}" if self.page_url.blank?
+    self.page_url = "/page#{@page_def_num+1}" if self.page_url.blank?
+  end
+
+  before_destroy do
+    unless self.is_childless?
+      errors.add(:base, "Cannot delete page with childrens") 
+      false
+    end
   end
   
-# валидации на наличие заголовка страницы, соответствии формату и требованиям по размеру (мин 3, макс 50)
+# validations for page title
   validates :page_title, presence: true, format: { with: VALID_NAME_REGEX }, length: { minimum: 3, maximum: 50 }, uniqueness: true
-# валидации на наличие ссылки страницы, в соответствии формату 
+# validations for page url
   validates :page_url, presence: true, format: { with: VALID_URL_REGEX }, uniqueness: true
-# валидации на длину описания 
+# validations for description 
   validates :description, length: { maximum: 200 }
 end
-
-
-
-
